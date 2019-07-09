7526b76a757b2db7b3bcb21d71998f3835bce9e4
kallaballa <amir@viel-zu.org>
2019-07-09 14:38:09 +0200

    tidying

---
+++ 1_prepareLimitedByElo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ELO=$1
+
+if [ -z "$ELO" ]; then
+	ELO=0;
+fi
+
+cut -d";" -f1,3,7,13,24 data/ladder.csv  | grep "\"[[:digit:]]*.gz\"" | sed 's/ /+/g;s/:/%3A/g;s/"//g' | while read line; do 
+	we="`echo "$line" | cut -d";" -f3`"; 
+	le="`echo "$line" | cut -d";" -f4`";
+	if [ $we -lt $ELO -o $le -lt $ELO ]; then
+		continue;
+	fi
+        winner="`echo "$line" | cut -d";" -f1`"; 
+        d="`echo "$line" | cut -d";" -f2`";
+	echo "$winner;$we;$le;http://wesnoth.gamingladder.info/download-replay.php?reported_on=$d"; 
+done > prepared.csv
--- 3_decide.sh
+++ 3_decide.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 ELOLIMIT="$1"
-set -x
 echo "winner_faction;loser_faction;winner;loser;title;file" > result.csv
 cat prepared.csv | while read line; do
 	file="$( basename "`echo "$line" | cut -d";" -f4`")" 
 	zcat "$file" | wml2xml > "$file.xml"
-	./decideByName.sh "`echo "$line" | cut -d";" -f1`" "$( basename "`echo "$line" | cut -d";" -f4`").xml" "`echo "$line" | cut -d";" -f3`" "`echo "$line" | cut -d";" -f2`" "$ELOLIMIT"; 
+	./decideByName.sh "`echo "$line" | cut -d";" -f1`" "$file.xml"; 
 done 2> errors.txt > result.csv
