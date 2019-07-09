f081d43872204c4ca55383f85d6622fb925334f4
kallaballa <amir@viel-zu.org>
2019-07-09 14:51:01 +0200

    removed obsolete code

--- 3_decide.sh
+++ 3_decide.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ELOLIMIT="$1"
 echo "winner_faction;loser_faction;winner;loser;title;file" > result.csv
 cat prepared.csv | while read line; do
 	file="$( basename "`echo "$line" | cut -d";" -f4`")" 
